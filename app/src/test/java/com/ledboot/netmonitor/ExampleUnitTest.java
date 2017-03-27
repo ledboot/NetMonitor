7a6eb4c6dd4bc9d2c1614dd2574c73442deb3001
ouyangxingyu <yangxingyu1217@163.com>
2017-03-27 09:58:07 +0800

    update test case

--- app/src/test/java/com/ledboot/netmonitor/ExampleUnitTest.java
+++ app/src/test/java/com/ledboot/netmonitor/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package com.ledboot.netmonitor;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import okhttp3.Headers;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,5 +18,29 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        Headers.Builder headerBuilder = new Headers.Builder();
+        resetHeader(headerBuilder);
+        System.out.println(headerBuilder.get("key1"));
+    }
+
+    /**
+     * 重置头部
+     * @param o
+     */
+    private void resetHeader(Object o){
+        try {
+            Method method = o.getClass().getDeclaredMethod("add",new Class[]{String.class,String.class});
+            if(method != null){
+                method.invoke(o,new String[]{"key1","123456"});
+            }else{
+                System.out.println("method is null!");
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
