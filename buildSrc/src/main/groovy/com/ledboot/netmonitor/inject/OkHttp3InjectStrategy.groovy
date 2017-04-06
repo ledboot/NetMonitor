45745db759e5e8df2da34c542b224165ceb3194a
ouyangxingyu <yangxingyu1217@163.com>
2017-04-06 15:19:21 +0800

    update inject

--- buildSrc/src/main/groovy/com/ledboot/netmonitor/inject/OkHttp3InjectStrategy.groovy
+++ buildSrc/src/main/groovy/com/ledboot/netmonitor/inject/OkHttp3InjectStrategy.groovy
@@ -21,21 +21,18 @@ public class OkHttp3InjectStrategy implements InjectStrategy {
     @Override
     void inject(ClassPool classPool, CtClass ctClass) {
         def className = ctClass.getClassFile().name
-        if(className.equals("okhttp3.Request\$Builder")) {
-            Object o = ctClass.getAnnotation(classPool.getCtClass(monitorAnnotation).toClass())
-            if (o == null) {
-                //给类增加注解
-                ClassFile cf = ctClass.getClassFile()
-                ConstPool cp = cf.getConstPool()
-                AnnotationsAttribute attr = new AnnotationsAttribute(cp, AnnotationsAttribute.visibleTag)
-                Annotation annotation = new Annotation(cp, classPool.getCtClass(monitorAnnotation))
-                attr.setAnnotation(annotation)
-                cf.addAttribute(attr)
-                cf.setVersionToJava5()
-                //方法注入Interceptor
-                CtMethod method = ctClass.getDeclaredMethod("headers")
-                method.insertBefore("com.ledboot.interceptor.HttpInterceptor.injectHeader(\$1);")
-            }
+        if (className.equals("okhttp3.Request\$Builder")) {
+            //给类增加注解
+            ClassFile cf = ctClass.getClassFile()
+            ConstPool cp = cf.getConstPool()
+            AnnotationsAttribute attr = new AnnotationsAttribute(cp, AnnotationsAttribute.visibleTag)
+            Annotation annotation = new Annotation(cp, classPool.getCtClass(monitorAnnotation))
+            attr.setAnnotation(annotation)
+            cf.addAttribute(attr)
+            cf.setVersionToJava5()
+            //方法注入Interceptor
+            CtMethod method = ctClass.getDeclaredMethod("headers")
+            method.insertBefore("com.ledboot.interceptor.HttpInterceptor.injectHeader(\$1);")
         }
     }
 }
